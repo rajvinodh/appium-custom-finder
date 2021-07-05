86483c5b36467c66321a7e700a832f8467c9601a
Vinith arjun <arjunvinith@gmail.com>
2021-07-05 20:02:33 +0530

    adding a test for initializing and updating pojo

--- src/main/java/recordings/JSONInitializers.java
+++ src/main/java/recordings/JSONInitializers.java
@@ -2,6 +2,7 @@ package recordings;
 
 import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.*;
+import recordings.pojo.*;
 
 import java.io.*;
 
@@ -9,17 +10,17 @@ public class JSONInitializers {
 
         ObjectMapper objectMapper;
         public ElementRecordPOJO elementRecordPOJO;
-        File jsonFile = new File("src/main/java/recordings/ImageRecordings/Login/login.json");
+        File jsonFile;
 
-        public JSONInitializers() throws IOException {
+        public JSONInitializers(File jsonFile) throws IOException {
             objectMapper = new ObjectMapper();
+            this.jsonFile = jsonFile;
             elementRecordPOJO = objectMapper.readValue(jsonFile, ElementRecordPOJO.class);
         }
 
         public String writeRequest() throws IOException {
             objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
             String request = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(elementRecordPOJO);
-//            logger.info("Request Json : \n" + request);
             try{
 
                 FileWriter fw = new FileWriter(jsonFile.getAbsoluteFile());
